0928f9103098553ad262f2dcb935abe15bee6628
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-11-21 23:14:45 +0300

    Добавил компоненты для изменения темы

---
+++ components/Theme/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { lucide_moon, lucide_sun } from "@/public/img/theme-icons"
+import { useTheme } from "next-themes"
+import { useEffect } from "react"
+import { Themes } from "./Themes"
+import "./ThemeSwitcher.css"
+
+const availableThemes: Themes = {
+	light: "light-theme",
+	dark: "dark-theme",
+}
+
+export default function ThemeSwitcher() {
+	const { theme, setTheme } = useTheme()
+
+	useEffect(() => {
+		setTheme(localStorage.getItem("theme") || "light")
+	}, [])
+
+	function switchTheme() {
+		setTheme(
+			theme === availableThemes.dark
+				? availableThemes.light
+				: availableThemes.dark
+		)
+
+		document.documentElement.classList.toggle(availableThemes.light)
+		document.documentElement.classList.toggle(availableThemes.dark)
+	}
+
+	return (
+		<button className="theme-switcher" onClick={switchTheme}>
+			{theme === availableThemes.dark ? lucide_sun : lucide_moon}
+		</button>
+	)
+}
---
+++ components/Theme/ThemeSwitcher/Themes.ts
@@ -0,0 +1,4 @@
+export type Themes = {
+	light: "light-theme"
+	dark: "dark-theme"
+}

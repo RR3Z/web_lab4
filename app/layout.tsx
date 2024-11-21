c946e2dfee871a19da89102d055e8b8ddae0115c
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-11-21 22:15:56 +0300

    Вынес метаданные главной страницы в page из layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import "@/styles/globals.css"
-import { Metadata } from "next"
 import { Roboto_Mono } from "next/font/google"
 
 const font_roboto = Roboto_Mono({ subsets: ["cyrillic"] })
-
-export const metadata: Metadata = {
-	title: "TODO List",
-	description: "VSTU - Web - Lab 4",
-}
 
 export default function RootLayout({
 	children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Header from "@/components/Header/Header"
 import TodoList from "@/components/TodoList/TodoList"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "TODO List",
+	description: "VSTU - Web - Lab 4",
+}
 
 export default function Home() {
 	return (

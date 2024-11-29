a054490f9ee3b933610bd81701dee20ddf544da0
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-11-29 14:10:55 +0300

    Добавил загрузку страницы и использование getCardById

--- components/CardEditForm/CardEditForm.tsx
+++ components/CardEditForm/CardEditForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { editCard } from "@/data/dataRequests"
+import { editCard, getCardById } from "@/data/dataRequests"
 import { useRouter } from "next/navigation"
 import React, { useEffect, useState } from "react"
 import "./CardEditForm.css"
@@ -10,26 +10,24 @@ export default function CardEditForm({ cardId }: { cardId: Number }) {
 	const [title, setTitle] = useState<string>("")
 	const [description, setDescription] = useState<string>("")
 	const [status, setStatus] = useState<boolean>(false)
+	const [loading, setLoading] = useState<boolean>(false)
 
 	const router = useRouter()
 
 	useEffect(() => {
-		async function getCard() {
-			const response = await fetch(`/api/cards/${cardId}`)
-			const cardData = await response.json()
+		async function loadCard() {
+			setLoading(true)
+			const cardData = await getCardById(Number(cardId))
 
 			if (cardData) {
 				setTitle(cardData.title)
 				setDescription(cardData.description)
 				setStatus(cardData.completed)
+				setLoading(false)
 			}
 		}
 
-		try {
-			getCard()
-		} catch (error) {
-			console.error(error)
-		}
+		loadCard()
 	}, [])
 
 	const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -50,44 +48,50 @@ export default function CardEditForm({ cardId }: { cardId: Number }) {
 	}
 
 	return (
-		<form onSubmit={handleFormSubmit} className="form-container">
-			<div className="card-title-field form-input-container">
-				<label htmlFor="title">Название</label>
-				<input
-					type="text"
-					id="title"
-					className="custom-input"
-					value={title}
-					style={{ border: hasError ? "1px solid red" : "" }}
-					onChange={handleTittleChange}
-				/>
-			</div>
+		<>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<form onSubmit={handleFormSubmit} className="form-container">
+					<div className="card-title-field form-input-container">
+						<label htmlFor="title">Название</label>
+						<input
+							type="text"
+							id="title"
+							className="custom-input"
+							value={title}
+							style={{ border: hasError ? "1px solid red" : "" }}
+							onChange={handleTittleChange}
+						/>
+					</div>
 
-			<div className="card-desc-field form-input-container">
-				<label htmlFor="description">Описание</label>
-				<textarea
-					id="description"
-					className="custom-textarea"
-					value={description}
-					onChange={event => setDescription(event.target?.value)}
-				/>
-			</div>
+					<div className="card-desc-field form-input-container">
+						<label htmlFor="description">Описание</label>
+						<textarea
+							id="description"
+							className="custom-textarea"
+							value={description}
+							onChange={event => setDescription(event.target?.value)}
+						/>
+					</div>
 
-			<div className="card-status-select form-input-container">
-				<label htmlFor="status">Статус</label>
-				<select
-					className="custom-select"
-					name="status"
-					value={status.toString()}
-					onChange={event => setStatus(event.target?.value === "true")}
-				>
-					<option value="true">Сделано</option>
-					<option value="false">Не cделано</option>
-				</select>
-			</div>
-			<button className="custom-btn" disabled={hasError} type="submit">
-				Изменить
-			</button>
-		</form>
+					<div className="card-status-select form-input-container">
+						<label htmlFor="status">Статус</label>
+						<select
+							className="custom-select"
+							name="status"
+							value={status.toString()}
+							onChange={event => setStatus(event.target?.value === "true")}
+						>
+							<option value="true">Сделано</option>
+							<option value="false">Не cделано</option>
+						</select>
+					</div>
+					<button className="custom-btn" disabled={hasError} type="submit">
+						Изменить
+					</button>
+				</form>
+			)}
+		</>
 	)
 }
